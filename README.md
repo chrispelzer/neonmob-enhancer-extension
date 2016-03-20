23327c6f5b64a68460044ceaf8b41bfe3476028d
Chris Pelzer <chrispelzer@gmail.com>
2016-03-19 22:14:49 -0400

    initial creation of neonmob enhancer

---
+++ README.md
@@ -0,0 +1,11 @@
+# NeonMob Enhancer
+
+Enhancing Neonmob
+ 
+## License
+
+Copyright (c) 2016 Chris Pelzer.
+
+Licensed under the MIT license.
+
+Generated using [Chrome Extension Boilerplate](https://github.com/voidberg/chrome-extension-boilerplate)
---
+++ src/js/background.js
@@ -0,0 +1,23 @@
+/**
+ * @file NeonMob Enhancer
+ * @name Background
+ *
+ * Extension client script code.
+ *
+ * @author Chris Pelzer <me@chrispelzer.com>
+ */
+
+'use strict';
+
+class ExtensionBackground {
+
+    constructor() {
+        this.events();
+    }
+
+    events() {
+
+    }
+}
+
+new ExtensionBackground();
