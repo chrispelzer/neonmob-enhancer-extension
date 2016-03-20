f4c09967508e00f680a33984bfb6f9eab52bf260
Chris Pelzer <chrispelzer@gmail.com>
2016-03-19 22:37:59 -0400

    only show the percentage sold if the free packs have sold out

--- src/js/client.js
+++ src/js/client.js
@@ -16,14 +16,17 @@ class ExtensionClient {
         if(setId > 0) {
             $.get('https://www.neonmob.com/api/setts/' + setId + '/', {format: 'json'})
                 .done(function (data) {
-                    $('#status--free-packs').append(
-                        '<div>' +
-                        data.percent_sold_out + '%' + ' of packs sold out'
-                        + '</div>'
-                        + '<div>' +
-                        data.free_packs_claimed_percent + '%' + ' of free packs claimed'
-                        + '</div>'
-                    );
+
+                    if(data.freebies_discontinued == 'null') {
+                        $('#status--free-packs').append(
+                            '<div>' +
+                            data.percent_sold_out + '%' + ' of packs sold out'
+                            + '</div>'
+                            + '<div>' +
+                            data.free_packs_claimed_percent + '%' + ' of free packs claimed'
+                            + '</div>'
+                        );
+                    }
                 });
         }
     }
