782d333bd7d5b578beff3a7e74534810cba14781
Chris Pelzer <chrispelzer@gmail.com>
2016-03-19 23:35:40 -0400

    If free packs are sold out then show the date they sold out on

--- src/js/client.js
+++ src/js/client.js
@@ -19,11 +19,23 @@ class ExtensionClient {
                     if(data.freebies_discontinued == null) {
                         $('#status--free-packs').append(
                             '<div>' +
-                            data.percent_sold_out + '%' + ' of packs sold out'
-                            + '</div>'
-                            + '<div>' +
-                            data.free_packs_claimed_percent + '%' + ' of free packs claimed'
-                            + '</div>'
+                            data.percent_sold_out + '%' + ' of packs sold out' +
+                            '</div>' +
+                            '<div>' +
+                            data.free_packs_claimed_percent + '%' + ' of free packs claimed' +
+                            '</div>'
+                        );
+                    }else{
+                        var free_soldout = new Date(data.freebies_discontinued);
+                        var months = [ "January", "February", "March", "April", "May", "June",
+                            "July", "August", "September", "October", "November", "December" ];
+                        $('#status--free-packs').append(
+                            '<div>' +
+                                'The free packs sold out on <br>' +
+                                months[free_soldout.getMonth()] + ' ' +
+                                free_soldout.getDay() + ', ' +
+                                free_soldout.getFullYear() +
+                            '</div>'
                         );
                     }
                 });
