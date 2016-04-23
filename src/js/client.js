fe36d5352019eb54f22a3f2aa63e23fa545e00b9
Chris Pelzer <chrispelzer@gmail.com>
2016-04-23 11:40:42 -0400

    set this to 71% for a little bit, 50% might have been too low.

--- src/js/client.js
+++ src/js/client.js
@@ -22,7 +22,7 @@ class ExtensionClient {
                         message = '<div>' +
                             data.percent_sold_out + '% Total Packs Claimed' +
                             '</div>';
-                        if(data.free_packs_claimed_percent < 50){
+                        if(data.free_packs_claimed_percent < 71){ 
                             message += '<div>' +
                                 data.free_packs_claimed_percent + '% Free Packs Claimed' +
                                 '</div>';
