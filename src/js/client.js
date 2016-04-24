19d5983e15c194407e20c9b0b0ae0df766075a27
Chris Pelzer <chrispelzer@gmail.com>
2016-04-24 17:08:52 -0400

    add total print count stats

--- src/js/client.js
+++ src/js/client.js
@@ -11,25 +11,33 @@
 
 class ExtensionClient {
     constructor() {
+        // Grab the set id integer from the data-set-id data attribute
         var setId = $('[data-set-id]').data('set-id');
-        if(setId > 0) {
+
+        // Did we find a setId?
+        if (setId > 0) {
+            // Get the Set data from the API based on the setId found
             $.get('https://www.neonmob.com/api/setts/' + setId + '/', {format: 'json'})
                 .done(function (data) {
                     var message = '';
 
-                    if(data.freebies_discontinued == null) {
+                    // Have the free packs been discontinued yet?
+                    if (data.freebies_discontinued == null) {
                         message = '<div>' +
                             data.percent_sold_out + '% Total Packs Claimed' +
                             '</div>';
-                        if(data.free_packs_claimed_percent < 71){ 
+
+                        // Don't show if Neonmob is already showing it
+                        if (!$('#status--free-packs')) {
                             message += '<div>' +
                                 data.free_packs_claimed_percent + '% Free Packs Claimed' +
                                 '</div>';
                         }
-                    }else{
+                    } else {
+                        // If the free packs were sold out then display the date they sold out on
                         var free_soldout = new Date(data.freebies_discontinued);
-                        var months = [ "January", "February", "March", "April", "May", "June",
-                            "July", "August", "September", "October", "November", "December" ];
+                        var months = ["January", "February", "March", "April", "May", "June",
+                            "July", "August", "September", "October", "November", "December"];
                         message = '<div>' +
                             'The free packs sold out on <br>' +
                             months[free_soldout.getMonth()] + ' ' +
@@ -37,14 +45,59 @@ class ExtensionClient {
                             free_soldout.getFullYear() +
                             '</div>';
                     }
+                    console.log('test');
+                    // Find the rarity area
+                    var core_stats = data.core_stats;
+                    var special_stats = data.special_stats;
+                    // Loop through each rarity
+                    core_stats.forEach(function (rarity) {
+                        // Find the stat span
+                        var stat = $('.text-rarity-' + rarity.class_name);
+                        var stat_html = stat.html();
 
-                    if($('#status--released')) {
+                        // Update the odd position
+                        stat.parent().find('small').css('top', 3);
+
+                        // Add total print data
+                        stat_html += '<div style="padding-left: 25px">' +
+                            'Total Print:' + rarity.total_prints +
+                            '</div>';
+                        stat.html(stat_html);
+                    });
+
+                    // Loop through each rarity
+                    special_stats.forEach(function (rarity) {
+                        // Find the stat span
+                        var stat = $('.text-rarity-' + rarity.class_name);
+                        var stat_html = stat.html();
+
+                        // Update the odd position
+                        stat.parent().find('small').css('top', 3);
+                        if(rarity.name == 'chase'){
+                            var stat_padding = 'padding-left: 75px';
+                        }else if(rarity.name == 'variant'){
+                            var stat_padding = 'padding-left: 83px';
+                        }
+                        // Add total print data
+                        stat_html += '<span style="' + stat_padding + '">' +
+                            '<br>Total Print:' + rarity.total_prints +
+                            '</span>';
+                        stat.html(stat_html);
+                    });
+
+                    // Find the correct status id selector to append the message too
+                    if ($('#status--released')) {
                         $('#status--released').append(message);
-                    } else if($('#status--free-packs')){
+                    } else if ($('#status--free-packs')) {
                         $('#status--free-packs').append(message);
-                    } else if($('#status--paid')) {
+                    } else if ($('#status--paid')) {
                         $('#status--paid').append(message);
                     }
+                })
+                .error(function (jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR);
+                    console.log(textStatus);
+                    console.log(errorThrown);
                 });
         }
     }
