ebc54231f99050a8db87df8349660d57adcc8b69
Chris Pelzer <chrispelzer@gmail.com>
2016-06-11 14:33:37 -0400

    fix variable misspelling that caused the date the pack sold out not to show and some grammar

--- src/js/client.js
+++ src/js/client.js
@@ -52,9 +52,8 @@ class ExtensionClient {
                     var free_soldout = new Date(data.freebies_discontinued);
                     soldOutDays = ExtensionClient.diffDays(new Date(data.released), free_soldout);
 
-                    message = 'The free packs sold out within<br>'+
-                        soldOutDays + ' day' +
-                        (soldOutdays >= 2 ? ' s' : '') + ' on ' +
+                    message = 'The free packs sold out within ' + soldOutDays + ' day' +
+                        (soldOutDays >= 2 ? 's' : '') + ' on <br>' +
                         months[free_soldout.getMonth()] + ' ' +
                         free_soldout.getDate() + ', ' +
                         free_soldout.getFullYear();
@@ -64,8 +63,7 @@ class ExtensionClient {
                 if(data.free_packs_available === false && data.packs_available === false) {
                     soldOutDays = ExtensionClient.diffDays(new Date(data.released), new Date(data.discontinued));
                     message = 'The set sold out within<br>' + soldOutDays + ' day' +
-                        (soldOutdays >= 2 ? 's' : '') +
-                        ' since its release';
+                        (soldOutDays >= 2 ? 's' : '') + ' of its release';
                 }
 
                 // Find the correct status id selector to append the message too
