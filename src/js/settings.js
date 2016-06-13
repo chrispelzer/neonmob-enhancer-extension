1a2b7349157874d4b1e79f0306671e26ba746edb
Chris Pelzer <chrispelzer@gmail.com>
2016-06-12 21:19:33 -0400

    localStorage for the settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,8 +1,3 @@
-import 'foundation-sites/js/foundation.core';
-import 'foundation-sites/js/foundation.util.mediaQuery';
-import 'foundation-sites/js/foundation.util.keyboard';
-import 'foundation-sites/js/foundation.util.motion';
-
 /**
  * @file NeonMob Enhancer
  * @name Client
@@ -11,12 +6,40 @@ import 'foundation-sites/js/foundation.util.motion';
  *
  * @author Chris Pelzer <me@chrispelzer.com>
  */
-'use strict';
 
-class ExtensionSettings {
-    constructor() {
+import 'foundation-sites/js/foundation.core';
+import 'foundation-sites/js/foundation.util.mediaQuery';
+import 'foundation-sites/js/foundation.util.keyboard';
+import 'foundation-sites/js/foundation.util.motion';
 
+!function ($) {
+    'use strict';
+
+    class ExtensionSettings {
+        constructor() {
+            // Default settings
+            var settings = {
+                'pack-info': true,
+                'count-info': true,
+                'special-info': true,
+            }
+
+            // Set the default settings to localStorage
+            _.forEach(settings, function (value, key) {
+                if (localStorage[key] == '') {
+                    localStorage[key] = value;
+                }
+            });
+
+            this.setEvents(settings);
+        }
+
+        setEvents(settings) {
+            $('#pack-info').on('change', function (e) {
+                console.log(e);
+            });
+        }
     }
-}
 
-new ExtensionSettings();
+    new ExtensionSettings();
+}
